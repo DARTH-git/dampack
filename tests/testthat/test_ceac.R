dbf800136c5dd3fcaf8024253c6db4a1b201ea3b
Caleb Easterly <caleb.easterly@gmail.com>
2018-12-10 17:40:43 -0600

    move ceac tests to test_ceac.R

--- tests/testthat/test_ceac.R
+++ tests/testthat/test_ceac.R
@@ -1,11 +1,19 @@
 context("ceac")
 library(dampack)
 
-# setup
+# test the class
+test_that("ceac has all methods we'd expect", {
+  current_methods <- as.vector(methods(class = ceac))
+  expected_methods <- c("plot.ceac", "summary.ceac")
+  expect_equal(current_methods, expected_methods)
+})
+
+# test class creation creation
+
+## setup
 source('load_test_data.R')
 psa_obj <- psa_results(costs, effectiveness, strategies)
 
-# tests
 test_that("result has classes 'data.frame' and 'ceac'", {
   c <- ceac(wtp, psa_obj)
   expect_true(inherits(c, "data.frame"))
@@ -20,3 +28,23 @@ test_that("handles missing strategy", {
   expect_equal(expected_generic_strat, obtained_generic_strat)
 })
 
+
+# test methods
+
+## summary
+test_that("message is correct in summary.ceac", {
+  c <- ceac(wtp, psa_obj)
+  msg <- capture.output(summary(c))
+  expected <- c("There was one switch in the optimal strategy.",
+                "At WTP = 51000 the optimal strategy changed from Radio to Chemo")
+  expect_equal(expected, msg)
+})
+
+## plot
+test_that("plot.ceac produces ggplot object", {
+  ceac_obj <- ceac(wtp, psa_obj)
+  gf <- plot(ceac_obj, frontier = TRUE)
+  expect_true(inherits(gf, "ggplot"))
+  gnof <- plot(ceac_obj, rontier = FALSE)
+  expect_true(inherits(gnof, "ggplot"))
+})
--- tests/testthat/test_ceac_methods.R
+++
@@ -1,8 +0,0 @@
-context('ceac')
-library(dampack)
-
-test_that("ceac has all methods we'd expect", {
-  current_methods <- as.vector(methods(class = ceac))
-  expected_methods <- c("plot.ceac", "summary.ceac")
-  expect_equal(current_methods, expected_methods)
-})

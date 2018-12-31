f420b0e41ea583d2637df45b5bcaff024fd4d3c5
Caleb Easterly <caleb.easterly@gmail.com>
2018-12-31 15:08:29 -0600

    simplify

--- R/calculate_icers.R
+++ R/calculate_icers.R
@@ -10,57 +10,51 @@
 calculate_icers <- function(cost, effect, strategies) {
   # todo: check data is in correct format
 
-  df <- data.frame("Strategy" = strategies, "Cost" = cost, "Effect" = effect,
+  df <- data.frame("Strategy" = strategies,
+                   "Cost" = cost,
+                   "Effect" = effect,
+                   "Status" = "ND",
                    stringsAsFactors = FALSE)
   coln <- colnames(df)
   nstrat <- nrow(df)
   # remove dominated strategies
+  # dominated strategies have a higher cost and lower effect
   df <- df %>%
     arrange(.data$Cost, desc(.data$Effect))
-  dominated <- df[FALSE, ]
   for (i in 1:(nstrat - 1)) {
+    ith_effect <- df[i, "Effect"]
     for (j in (i + 1):nstrat) {
-      if (df[j, "Effect"] <= df[i, "Effect"]) {
-        dominated <- union(dominated, df[j, ])
+      jth_effect <- df[j, "Effect"]
+      if (jth_effect <= ith_effect) {
+        df[j, "Status"] <- "D"
       }
     }
   }
-  non_dominated <- setdiff(df, dominated)
 
-  # remove extendedly dominated strategies
-  # extended dominance means ICER > that of both previous and next strategies in order of cost
-  ext_dominated <- non_dominated[FALSE, ]
-  if (nrow(non_dominated) > 3) {
-    ext_dominated_count <- nrow(ext_dominated)
-    prev_ext_dominated_count <- -1 # run atleast once
-    while (ext_dominated_count > prev_ext_dominated_count &
-           ( (nrow(non_dominated) - ext_dominated_count) > 3) ) {
-      non_dominated_set <- compute_icers(setdiff(non_dominated, ext_dominated))
-      prev_ext_dominated_count <- ext_dominated_count
-      for (i in 3:(nrow(non_dominated_set) - 1)) {
-        if (non_dominated_set[i, "ICER"] > non_dominated_set[i - 1, "ICER"] &
-            non_dominated_set[i, "ICER"] > non_dominated_set[i + 1, "ICER"]){
-          ext_dominated <- union(ext_dominated, non_dominated_set[i, coln])
-        }
-      }
-      ext_dominated_count <- nrow(ext_dominated)
+  # detect weakly dominated strategies (extended dominance)
+  non_d <- df[df$Status != "D", ] %>%
+    compute_icers()
+  n_non_d <- nrow(non_d)
+  for (i in 2:(n_non_d - 1)) {
+    if (non_d[i, "ICER"] > non_d[i + 1, "ICER"]) {
+      non_d[i, "Status"] <- "ED"
+      non_d[i, "ICER"] <- NA
     }
   }
-
-  # calculate ICERs for those strategies not dominated
-  # nor etendedly dominated
-  non_ext_dominated <- setdiff(non_dominated, ext_dominated) %>%
-    compute_icers() %>%
-    mutate(Status = "ND")
-
-  dominated <- dominated %>%
-    mutate(ICER = NA, Status = "D")
-
-  ext_dominated <- ext_dominated %>%
-    mutate(ICER = NA, Status = "ED")
+  # recompute icers without weakly dominated strategies
+  non_ed <- non_d[non_d$Status != "ED", ] %>%
+    compute_icers()
 
   # recombine all results to produce final output
-  results <- bind_rows(non_ext_dominated, dominated, ext_dominated)
+  d <- df[df$Status == "D", ]
+  ed <- non_d[non_d$Status == "ED", ]
+
+  # when combining, sort so we have ref,ND,ED,D
+  results <- bind_rows(d, ed, non_ed) %>%
+    arrange(desc(.data$Status), .data$Cost, desc(.data$Effect))
+
+  # declare status for first entry to be 'ref'
+  results[1, "Status"] <- "ref"
 
   # declare class of results
   class(results) <- c("icers", "data.frame")
@@ -69,16 +63,13 @@ calculate_icers <- function(cost, effect, strategies) {
 
 
 # Source: https://miqdad.freeasinspeech.org.uk/icer_calculator/
-compute_icers <- function(non_dominated) {
-  icers <- non_dominated %>%
-    arrange(.data$Cost, desc(.data$Effect))
-
-  if (nrow(non_dominated) > 1) {
-    icers[1, "ICER"] <- NA
-    for (i in 2:nrow(non_dominated)) {
-      icers[i, "ICER"] <- (icers[i, "Cost"] - icers[i - 1, "Cost"]) /
-        (icers[i, "Effect"] - icers[i - 1, "Effect"])
+compute_icers <- function(non_d) {
+  if (nrow(non_d) > 1) {
+    non_d[1, "ICER"] <- NA
+    for (i in 2:nrow(non_d)) {
+      non_d[i, "ICER"] <- (non_d[i, "Cost"] - non_d[i - 1, "Cost"]) /
+        (non_d[i, "Effect"] - non_d[i - 1, "Effect"])
     }
   }
-  return(icers)
+  return(non_d)
 }

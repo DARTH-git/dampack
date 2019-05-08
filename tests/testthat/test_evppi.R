4c3b232149b307784a49bdb443da143595a79d99
Caleb Easterly <caleb.easterly@gmail.com>
2019-05-08 15:57:09 -0500

    add expect silent

--- tests/testthat/test_evppi.R
+++ tests/testthat/test_evppi.R
@@ -8,7 +8,9 @@ data("example_psa")
 psa_obj <- make_psa_obj(example_psa$cost, example_psa$effectiveness,
                         example_psa$parameters, example_psa$strategies)
 
-calc_evppi(wtp = 1e5,
-           psa = psa_obj,
-           parms = c("pFailSurg", "pFailChemo"),
-           outcome = "nmb_loss")
+test_that("evppi runs", {
+  expect_silent(calc_evppi(wtp = 1e5,
+                           psa = psa_obj,
+                           parms = c("pFailSurg", "pFailChemo"),
+                           outcome = "nmb_loss"))
+})

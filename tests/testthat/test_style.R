34daf6005e8c2dde9a184a14c0f296f5765c5180
gknowlt <gsknowlton@gmail.com>
2019-11-27 16:53:14 -0600

    implement lintr fix
    lintr test will no longer run when check is initiated through RStudio IDE. lintr test will still run within Travis-CI and tests

--- tests/testthat/test_style.R
+++ tests/testthat/test_style.R
@@ -1,6 +1,25 @@
-library(lintr)
+if (requireNamespace("lintr", quietly = TRUE)) {
+  library(lintr)
 
-context("lints")
-test_that("Package Style", {
-  lintr::expect_lint_free()
-})
+  context("lints")
+  test_that("Package Style", {
+
+    # A duplicate copy of the find_package function from lintr
+    find_package <- function(path) {
+      path <- normalizePath(path, mustWork = FALSE)
+
+      while (!file.exists(file.path(path, "DESCRIPTION"))) {
+        path <- dirname(path)
+        if (identical(path, dirname(path))) {
+          return(NULL)
+        }
+      }
+
+      path
+    }
+
+    if (!is.null(find_package("."))) {
+      expect_lint_free()
+    }
+  })
+}
